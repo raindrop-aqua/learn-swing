5b40e8f7caa39b64b68daad27bb7b0f6b10be61e
Masahiro Atsumi <raindro.aqua@gmail.com>
2017-12-13 23:58:17 +0900

    add ch4.1

---
+++ swing01/src/com/lean/c04_01/TJFrame.java
@@ -0,0 +1,40 @@
+package com.lean.c04_01;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class TJFrame extends JFrame {
+
+    Container container = null;
+
+    public static void main(String[] args) {
+        TJFrame frame = new TJFrame("Swing Frame");
+    }
+
+    public TJFrame(String title) {
+        super(title);
+
+        container = getContentPane();
+        container.setBackground(Color.white);
+        container.setForeground(Color.blue);
+
+        JLabel label = new JLabel("This is Swing frame", JLabel.CENTER);
+        label.setFont(new Font("Sans", Font.PLAIN, 22));
+        container.add(label);
+
+        addWindowListener(new WindowEventHandler());
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(350, 200);
+        setVisible(true);
+    }
+
+    private class WindowEventHandler extends WindowAdapter {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
+        }
+    }
+}
